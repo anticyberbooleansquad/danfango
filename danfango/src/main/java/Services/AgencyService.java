a38c66366ab23128bb1b4674bf68ddef7d51b1c4
Joeg332 <joseph.giardina@stonybrook.edu>
2017-04-13 20:09:06 -0400

    parsing xml file on the danfango server i must still add set methods and some attributes on the models

--- danfango/src/main/java/Services/AgencyService.java
+++ danfango/src/main/java/Services/AgencyService.java
@@ -3,32 +3,87 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package Services;
 
+import Model.Movie;
+import java.io.File;
+import java.io.IOException;
+import java.sql.Timestamp;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import org.dom4j.Document;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
 /**
- * 
+ *
  * @author Sammy Guergachi <sguergachi at gmail.com>
  */
 @Service
 public class AgencyService {
 
     @Autowired
-    MemberService memberService;
-    public AgencyService(){
-    }
-    
-    
-    public static void parseMovieFile(){
-        try{
-            File inputFile = new File("movieAgency.xml");
-         DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
-         DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
-         Document doc = dBuilder.parse(inputFile);
-         doc.getDocumentElement().normalize();
-         System.out.println("Root element :" + doc.getDocumentElement().getNodeName());
-         NodeList nList = doc.getElementsByTagName("student");
+    MovieService movieService;
 
+    public AgencyService() {
+    }
+
+    public void parseMovieFile() throws ParserConfigurationException, SAXException, IOException, ParseException {
+        File inputFile = new File("movieAgency.xml");
+        DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+        org.w3c.dom.Document doc = dBuilder.parse(inputFile);
+        doc.getDocumentElement().normalize();
+        NodeList nList = doc.getElementsByTagName("movie");
+
+        for (int counter = 0; counter < nList.getLength(); counter++) {
+            Node nNode = nList.item(counter);
+            if (nNode.getNodeType() == Node.ELEMENT_NODE) {
+                Element eElement = (Element) nNode;
+
+                // if the movie does not exist in our db then we add it
+                Movie movie = new Movie();
+
+                SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss.SSS");
+                Date parsedDate = dateFormat.parse(eElement.getElementsByTagName("released").item(0).getTextContent());
+                Timestamp timestamp = new java.sql.Timestamp(parsedDate.getTime());
+                movie.setReleaseDate(timestamp);
+                movie.setAgencyMovieId(Integer.parseInt(eElement.getElementsByTagName("imdbID").item(0).getTextContent()));
+                movie.setTitle(eElement.getElementsByTagName("title").item(0).getTextContent());
+                movie.setSynopsis(eElement.getElementsByTagName("plot").item(0).getTextContent());
+
+                // if movie does not exist then we add the movie
+                if (movieService.getMovieByAgencyId(counter) == null) {
+                    movieService.addMovie(movie);
+                } // if the movie does exist then we update that movie oobject
+                else {
+                    movieService.updateMovie(movie);
+                }
+
+                System.out.println("title : " + eElement.getElementsByTagName("title").item(0).getTextContent());
+                System.out.println("year : " + eElement.getElementsByTagName("year").item(0).getTextContent());
+                System.out.println("rated : " + eElement.getElementsByTagName("rated").item(0).getTextContent());
+                System.out.println("released : " + eElement.getElementsByTagName("released").item(0).getTextContent());
+                System.out.println("imdbID : " + eElement.getElementsByTagName("imdbID").item(0).getTextContent());
+                System.out.println("imdbRating : " + eElement.getElementsByTagName("imdbRating").item(0).getTextContent());
+                System.out.println("genre : " + eElement.getElementsByTagName("genre").item(0).getTextContent());
+                System.out.println("plot : " + eElement.getElementsByTagName("plot").item(0).getTextContent());
+                System.out.println("poster : " + eElement.getElementsByTagName("poster").item(0).getTextContent());
+                System.out.println("runtime : " + eElement.getElementsByTagName("runtime").item(0).getTextContent());
+                System.out.println("actors : " + eElement.getElementsByTagName("actors").item(0).getTextContent());
+                System.out.println("director : " + eElement.getElementsByTagName("director").item(0).getTextContent());
+                System.out.println("writer : " + eElement.getElementsByTagName("writer").item(0).getTextContent());
+                System.out.println("\n");
+
+            }
         }
     }
 }
