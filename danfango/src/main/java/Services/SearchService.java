4c9a691f03f82eee72e90c1eb3f9ec38ac1c59a3
kjuszkiewicz <konrad.juszkiewicz@stonybrook.edu>
2017-04-21 23:54:24 -0400

    Removed unused imports.

--- danfango/src/main/java/Services/SearchService.java
+++ danfango/src/main/java/Services/SearchService.java
@@ -11,24 +11,13 @@ import Model.LocationSearchResult;
 import Model.Movie;
 import Model.SearchResults;
 import Model.Theatre;
-import com.google.common.collect.BiMap;
-import com.google.common.collect.ImmutableBiMap;
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.net.MalformedURLException;
-import java.net.URL;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.json.JSONArray;
-import org.json.JSONObject;
 
 /**
  *
